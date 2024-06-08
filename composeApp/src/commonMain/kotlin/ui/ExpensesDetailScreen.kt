f9a09796437bd8f5af47a6e72195974566cbfc3f
suarezfc82@gmail.com <suarezfc82@gmail.com>
2024-06-07 19:24:50 -0500

    amount_price_detail

--- composeApp/src/commonMain/kotlin/ui/ExpensesDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/ExpensesDetailScreen.kt
@@ -4,18 +4,27 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.BottomSheetState
+import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -32,7 +41,15 @@ import model.Expense
 import model.ExpenseCategory
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.SoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 
 
@@ -79,8 +96,106 @@ fun ExpensesDetailScreen(
             }
         }
     ) {
+        Column(modifier = Modifier.fillMaxSize().padding(vertical = 16.dp, horizontal = 16.dp)) {
+
+            ExpenseAmount(priceContent = price,
+                onPriceChange = {
+                    price = it
+                }, keyboardController = keyboardController
+                )
+            Spacer(modifier = Modifier.height(30.dp))
+
+        }
+
 
     }
+}
+
+@Composable
+private fun ExpenseAmount(
+    priceContent: Double,
+    onPriceChange: (Double) -> Unit,
+    keyboardController: SoftwareKeyboardController?
+) {
+    val colors = getColorsTheme()
+    var text by remember {
+        mutableStateOf("$priceContent")
+    }
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "Amount",
+            fontSize = 20.sp,
+            color = Color.Gray,
+            fontWeight = FontWeight.SemiBold
+        )
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = text,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.ExtraBold,
+                color = colors.textColor
+            )
+
+            TextField(
+                modifier = Modifier.weight(1f),
+                value = text,
+                onValueChange = { newText ->
+                    val numericText = newText.filter { it.isDigit() || it == '.' }
+                    text = if (numericText.isNotEmpty() && numericText.count { it == '.' } <= 1) {
+                        try {
+                            val newValue = numericText.toDouble()
+                            onPriceChange(newValue)
+                            numericText
+
+                        } catch (e: NumberFormatException) {
+                            ""
+                        }
+                    } else {
+                        onPriceChange(0.0)
+                        ""
+                    }
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Decimal,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        keyboardController?.hide()
+                    }
+                ),
+
+                singleLine = true,
+                colors = TextFieldDefaults.textFieldColors(
+
+                    textColor = colors.textColor,
+                    backgroundColor = colors.backGroundColor,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    unfocusedLabelColor = Color.Transparent
+                ),
+                textStyle = TextStyle(fontSize = 35.sp, fontWeight = FontWeight.ExtraBold)
+            )
+
+            Text(
+                text = "USD",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.ExtraBold,
+                color = Color.Gray
+            )
+        }
+
+        Divider(color = Color.Black, thickness = 2.dp)
+
+    }
+
 }
 
 @Composable
